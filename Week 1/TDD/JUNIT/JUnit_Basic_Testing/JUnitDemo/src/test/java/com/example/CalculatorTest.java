de91912d817f3cee3a7e96aebcff7a88e22a7ffe
Sai Venkatesh Ramisetti <saivenkateshramisetti@gmail.com>
2026-06-23 23:04:26 +0530

    Refactor CalculatorTest with setup and teardown methods

--- Week 1/TDD/JUNIT/JUnit_Basic_Testing/JUnitDemo/src/test/java/com/example/CalculatorTest.java
+++ Week 1/TDD/JUNIT/JUnit_Basic_Testing/JUnitDemo/src/test/java/com/example/CalculatorTest.java
@@ -1,29 +1,3 @@
-//package com.example;
-//
-//import org.junit.Test;
-//import static org.junit.Assert.assertEquals;
-//
-//public class CalculatorTest {
-//
-//    @Test
-//    public void testAdd() {
-//        Calculator calculator = new Calculator();
-//
-//        int result = calculator.add(5, 3);
-//
-//        assertEquals(8, result);
-//    }
-//    @Test
-//    public void testSubtract() {
-//        Calculator calculator = new Calculator();
-//
-//        int result = calculator.subtract(6, 2);
-//
-//        assertEquals(4, result);
-//    }
-//}
-//
-
 package com.example;
 
 import org.junit.Before;
