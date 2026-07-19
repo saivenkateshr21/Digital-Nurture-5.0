04238c8900ad9f95e89c3e047550347c71880400
Sai Venkatesh Ramisetti <saivenkateshramisetti@gmail.com>
2026-07-20 00:36:39 +0530

    Add files via upload

---
+++ Week 6/9.ReactJS_hol/cricketapp/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ListofPlayers from "./ListofPlayers";
+import IndianPlayers from "./IndianPlayers";
+
+function App() {
+
+    const flag = true;
+
+    if (flag) {
+        return <ListofPlayers />;
+    }
+    else {
+        return <IndianPlayers />;
+    }
+
+}
+
+export default App;
